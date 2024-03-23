3ad0cb23bc31399df929dbf256c4125a9064868c
ilya_stezhko@mail.ru <ilya_stezhko@mail.ru>
2024-03-23 13:32:58 +0300

    Add files

---
+++ backend/users/migrations/0002_auto_20240301_1206.py
@@ -0,0 +1,20 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('users', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='user',
+            name='role',
+        ),
+        migrations.AlterField(
+            model_name='user',
+            name='password',
+            field=models.CharField(max_length=128, verbose_name='password'),
+        ),
+    ]
